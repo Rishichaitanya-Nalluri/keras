ed6139c6cd735b2712430db1ab03a2ead99410ba
N. Rishi Chaitanya Sri Prasad <94001904+Rishichaitanya-Nalluri@users.noreply.github.com>
2025-04-28 07:48:47 +0530

    Update __init__.py

--- keras/src/layers/activations/__init__.py
+++ keras/src/layers/activations/__init__.py
@@ -3,3 +3,4 @@ from keras.src.layers.activations.leaky_relu import LeakyReLU
 from keras.src.layers.activations.prelu import PReLU
 from keras.src.layers.activations.relu import ReLU
 from keras.src.layers.activations.softmax import Softmax
+from keras.src.layers.activations.softmax import LogLU
